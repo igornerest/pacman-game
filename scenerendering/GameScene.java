f4d0ad2994cff61e41a8f8787a26352cd3553f19
igornerest <igornerest@gmail.com>
2018-07-22 14:37:35 -0300

    add Ghost object as attribute and setPlayer() method

--- scenerendering/GameScene.java
+++ scenerendering/GameScene.java
@@ -18,18 +18,19 @@ public class GameScene{
 	
 	private Map gameMap;
 
-	private Pacman usrPacman;
+	private Player[] gamePlayers;
 
 	public GameScene(int screenWidth, int screenHeight, EventHandler<KeyEvent> value, int selectedLevel) {
+		Group gameRoot = new Group();
 		// according to the selected level, newMap creates all the imageView objects
 		// and stores them in a ObservableList
 		gameMap = new Map(Preferences.LEVEL_SRC[selectedLevel]);
-		
-		Group gameRoot = new Group(gameMap.getTilesList());
-		
-		usrPacman = new Pacman(1, 1);
-		gameRoot.getChildren().add(usrPacman.getPlayerImage());
+		gameRoot.getChildren().addAll(gameMap.getTilesList());
 
+		setPlayers();
+		for(int i = 0; i < 5; i++) 
+			gameRoot.getChildren().add(gamePlayers[i].getPlayerImage());
+	
 		// The AnimationTimer's handle method is invoked once for each frame 
 		// that is rendered, on the FX Application Thread. 
 		// We should never block that thread (calling Thread.sleep(...)) here.
@@ -41,7 +42,7 @@ public class GameScene{
             @Override
             public void handle(long now) {
             	if(now - lastUpdate >= Preferences.NSPF) {	// nanoseconds per frame
-	            	usrPacman.move(gameMap);
+	            	movePlayers();
 	            	lastUpdate = now;
 	            }
             }
@@ -52,8 +53,22 @@ public class GameScene{
 		this.gameScene.setOnKeyPressed(value);
 	}
 
+	private void movePlayers() {
+		for(int i = 0;  i < 5; i++)
+			this.gamePlayers[i].move(this.gameMap);
+	}
+
+	private void setPlayers() {
+		gamePlayers = new Player[5];
+		gamePlayers[0] = new Pacman(1,1);
+		gamePlayers[1] = new Ghost(1, 2, Preferences.YELLOW_GHOST_SRC);
+		gamePlayers[2] = new Ghost(1, 3, Preferences.BLUE_GHOST_SRC);
+		gamePlayers[3] = new Ghost(1, 4, Preferences.GREEN_GHOST_SRC);
+		gamePlayers[4] = new Ghost(1, 5, Preferences.RED_GHOST_SRC);
+	}
+
 	public Player getPacman() {
-		return this.usrPacman;
+		return this.gamePlayers[0];
 	}
 
 	public void setScene(Stage stage) {
