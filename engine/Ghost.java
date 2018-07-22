187613340513c78717506e21a1fc4b009a5ca36b
igornerest <igornerest@gmail.com>
2018-07-22 14:36:19 -0300

    add image source to constructor

--- engine/Ghost.java
+++ engine/Ghost.java
@@ -2,7 +2,7 @@ package engine;
 
 public class Ghost extends Player {
 
-	public Ghost(int xPos, int yPos) {
-		super(" ", xPos, yPos);
+	public Ghost(int xPos, int yPos, String imgSource) {
+		super(imgSource, xPos, yPos);
 	}
 }
