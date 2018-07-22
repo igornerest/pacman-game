8c61b311b8bad06d141a59e0dba61f62d85d8527
igornerest <igornerest@gmail.com>
2018-07-22 14:59:31 -0300

    add method to verify ghosts in the same position

--- engine/Pacman.java
+++ engine/Pacman.java
@@ -5,4 +5,14 @@ public class Pacman extends Player {
 	public Pacman(int xPos, int yPos) {
 		super(Preferences.PACMAN_SRC, xPos, yPos);
 	}
+
+	private boolean hasSamePosition(Ghost ghost) {
+		return this.getXPos() == ghost.getXPos() && this.getYPos() == ghost.getYPos();
+	}
+
+	public void verifyGhosts(Ghost ... ghosts) {
+		for(Ghost ghost : ghosts)
+			if(this.hasSamePosition(ghost))
+				this.setDeath();
+	}
 }
