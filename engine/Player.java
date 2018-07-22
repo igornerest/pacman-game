d6702d6d063774d86a57f49d90411161bb14c969
igornerest <igornerest@gmail.com>
2018-07-22 12:18:14 -0300

    smooth movement - add tryMovement(...) method

--- engine/Player.java
+++ engine/Player.java
@@ -11,13 +11,14 @@ public abstract class Player {
 
 	private ImageView playerImage;
 
-	private String movement;
+	private String movement, nextMovement;
 
 	public Player(String imgSource, int xPos, int yPos) {
 		this.xPos = xPos;
 		this.yPos = yPos; 
 		this.movement = "STOPPED";
-	
+		this.nextMovement = "STOPPED";
+
 		try (FileInputStream fileStream = new FileInputStream(imgSource)) {
 			this.playerImage = new ImageView(new Image(fileStream));
 			this.playerImage.setPreserveRatio(true);
@@ -25,68 +26,72 @@ public abstract class Player {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		updateImagePosition();
+		updateImagePosition(0);
 	} 
 
-	private void updateImagePosition() {
+	private void updateImagePosition(int imageRotation) {
 		// sets x and y ImageView positions according to current Player positions
 		this.playerImage.setY(this.yPos * Preferences.TILE_SIZE);
 		this.playerImage.setX(this.xPos * Preferences.TILE_SIZE);
+		this.playerImage.setRotate(imageRotation);
 	}
 
 	public ImageView getPlayerImage() {
 		return this.playerImage;
 	}
 
-	private void checknUpdate(Map map, int xMove, int yMove) {
+	private boolean checknUpdate(Map map, int xMove, int yMove, int imgRotation) {
 		// checks if there's path to move
 		// if so, updates player's positions
 		if(map.isPath(xMove, yMove)) {
 			this.xPos = xMove;
 			this.yPos= yMove;
-			updateImagePosition();
+			updateImagePosition(imgRotation);
+			return true;
 		}
+		return false;
 	}
 	
-	public void move(Map map) {
-		// player moves everytime AnimationTimer ticks, according to player's movement status
-		switch(this.movement) {
+	private boolean tryMovement(Map map, String movement) {
+		switch(movement) {
 			case "UP":
-				checknUpdate(map, this.xPos, this.yPos - 1);
-				break;
+				return checknUpdate(map, this.xPos, this.yPos - 1, -90);
 
 			case "DOWN":
-				checknUpdate(map, this.xPos, this.yPos + 1);
-				break;
+				return checknUpdate(map, this.xPos, this.yPos + 1, 90);
 
 			case "RIGHT":
-				checknUpdate(map, this.xPos + 1, this.yPos);
-				break;
+				return checknUpdate(map, this.xPos + 1, this.yPos, 0);
 
 			case "LEFT":
-				checknUpdate(map, this.xPos - 1, this.yPos);
-				break;
+				return checknUpdate(map, this.xPos - 1, this.yPos, 180);
+
+			default: 
+				return false;
 		}
+	}
+
+	public void move(Map map) {
+		if(tryMovement(map, this.nextMovement))
+			this.movement = this.nextMovement;
+		else
+			tryMovement(map, this.movement);
 	}
 
 	// Following methods update player's movement status
 	public void setUp() {
-		this.movement = "UP";
-		this.playerImage.setRotate(-90); 
+		this.nextMovement = "UP"; 
 	}
 
 	public void setDown() {
-		this.movement = "DOWN";
-		this.playerImage.setRotate(90); 
+		this.nextMovement = "DOWN";
 	}
 
 	public void setRight() {
-		this.movement = "RIGHT";
-		this.playerImage.setRotate(0); 
+		this.nextMovement = "RIGHT";
 	}
 
 	public void setLeft() {
-		this.movement = "LEFT";
-		this.playerImage.setRotate(180); 
+		this.nextMovement = "LEFT"; 
 	}
 }
