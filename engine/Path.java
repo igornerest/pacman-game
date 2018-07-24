f15c30e1efaf542516a54e8da661470d1455e576
igornerest <igornerest@gmail.com>
2018-07-24 18:43:38 -0300

    Pacman now eats. He is alivegit add engine/Tile.java engine/Path.java!

--- engine/Path.java
+++ engine/Path.java
@@ -1,12 +1,25 @@
 package engine;
 
+import java.io.FileInputStream;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Path extends Tile {
 	
 	boolean hasFood;
 
 	// Exception caught from Tile constructor
 	public Path(boolean hasFood, int x, int y) throws Exception {
-		super("./images/cleared.png", x, y);
+		super(Preferences.PATH_FOOD_SRC, x, y);
 		this.hasFood = hasFood;
+
+		if(!this.hasFood)
+			this.setCleared();
+	}
+
+	public void setCleared() {
+		if(hasFood)
+			this.setTileImageView(Preferences.PATH_CLEARED_SRC);
 	}
 }
--- engine/Tile.java
+++ engine/Tile.java
@@ -11,21 +11,24 @@ public abstract class Tile {
 
 	// method may throw an exception if the source of the Image is wrong
 	public Tile(String imgSource, int x, int y) throws Exception {
-	
-		try (FileInputStream fileStream = new FileInputStream(imgSource)) {
-			// setting image according to source of the file
-			this.tileImage = new ImageView(new Image(fileStream));
-			// setting x and y positions
-			this.tileImage.setX(x * Preferences.TILE_SIZE);
-			this.tileImage.setY(y * Preferences.TILE_SIZE);
-			this.tileImage.setPreserveRatio(true);
-			this.tileImage.setFitHeight(Preferences.TILE_SIZE);
-		} catch(Exception e) {
-			System.out.println(e);
-		}
+		this.tileImage = new ImageView();
+		this.setTileImageView(imgSource);
+		// setting x and y positions
+		this.tileImage.setX(x * Preferences.TILE_SIZE);
+		this.tileImage.setY(y * Preferences.TILE_SIZE);
+		this.tileImage.setPreserveRatio(true);
+		this.tileImage.setFitHeight(Preferences.TILE_SIZE);
 	}
 
-	// reinforcing good encapsulation 
+	public void setTileImageView(String imgSource) {
+		try (FileInputStream fileStream = new FileInputStream(imgSource)){
+			Image newImage = new Image(fileStream);
+			this.tileImage.setImage(newImage);
+		} catch(Exception e) {
+			System.out.println(e);
+		} 
+	}
+
 	public ImageView getTileImageView () {
 		return this.tileImage;
 	}
