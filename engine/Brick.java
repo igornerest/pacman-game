e2baf9c021047f61c284b12a9585b1d8c04732f6
igornerest <igornerest@gmail.com>
2018-08-01 00:27:25 -0300

    Contructor throws ScreenException

--- engine/Brick.java
+++ engine/Brick.java
@@ -1,9 +1,11 @@
 package engine;
 
+import scenerendering.ScreenException;
+
 public class Brick extends Tile {
 
 	// Exception caught from Tile constructor
-	public Brick(int x, int y) throws Exception {
+	public Brick(int x, int y) throws ScreenException {
 		super("./images/brick.png", x, y);
 	}
 }
