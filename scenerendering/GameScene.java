4c43fff4d4b2d49b2f481bd8c1d0ed684817a3ef
igornerest <igornerest@gmail.com>
2018-08-01 00:06:51 -0300

    add specific handle() method. Delete setScene(). Constructor correclty throws ScreenException

--- scenerendering/GameScene.java
+++ scenerendering/GameScene.java
@@ -2,40 +2,45 @@ package scenerendering;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
 import javafx.scene.Group; 
 import javafx.scene.Scene; 
 import javafx.stage.Stage;
 
-import java.io.File;		// for reading .txt document
-import java.util.Scanner;	// for getting user input/read .txt document
+import java.io.File;  // for reading .txt document
+import java.util.Scanner;  // for getting user input/read .txt document
 
-import engine.*;	// my package
+import engine.*;  // my package
 
 // timer used to keep player's movement
 // src : https://stackoverflow.com/questions/29962395/how-to-write-a-keylistener-for-javafx
 import javafx.animation.AnimationTimer;
 
-public class GameScene{
+public class GameScene implements IScene, EventHandler<KeyEvent> {
 	private Scene gameScene; // A scene represents the physical contents of a JavaFX application
 	
 	private Map gameMap;
 
 	private Player[] gamePlayers;
 
-	private boolean gameOver = false;
+	private boolean gameOver;
 
-	public GameScene(int screenWidth, int screenHeight, EventHandler<KeyEvent> value, int selectedLevel) {
+	public GameScene(int screenWidth, int screenHeight, String levelSource) {
 		Group gameRoot = new Group();
 		// according to the selected level, newMap creates all the imageView objects
 		// and stores them in a ObservableList
-		gameMap = new Map(Preferences.LEVEL_SRC[selectedLevel]);
-		gameRoot.getChildren().addAll(gameMap.getTilesList());
+		try {
+			gameMap = new Map(levelSource);
+			gameRoot.getChildren().addAll(gameMap.getTilesList());
 
-		setPlayers(Preferences.LEVEL_SRC[selectedLevel]);
-		for (int i = 0; i < 5; i++) 
-			gameRoot.getChildren().add(gamePlayers[i].getPlayerImage());
-	
+			setPlayers(levelSource);
+			for (int i = 0; i < 5; i++) 
+				gameRoot.getChildren().add(gamePlayers[i].getPlayerImageView());
+			
+		} catch(ScreenException e) {
+			e.handleException();
+		}
 		// The AnimationTimer's handle method is invoked once for each frame 
 		// that is rendered, on the FX Application Thread. 
 		// We should never block that thread (calling Thread.sleep(...)) here.
@@ -46,19 +51,58 @@ public class GameScene{
             // We want to throttle updates so they don't happen more than once every time
             @Override
             public void handle(long now) {
-            	if (now - lastUpdate >= Preferences.NSPF) {	// nanoseconds per frame
-	            	movePlayers();
-	            	lastUpdate = now;
+            	if (now - lastUpdate >= 90000000) {	// nanoseconds per frame
+            		try {
+	            		movePlayers();
+	            		lastUpdate = now;
+	            	} catch(ScreenException e) {
+						e.handleException();
+					}
 	            }
             }
         };
-    	timer.start();
-			
+		timer.start();
+
 		this.gameScene = new Scene(gameRoot, screenWidth, screenHeight);
-		this.gameScene.setOnKeyPressed(value);
+		this.gameScene.setOnKeyPressed(this);
 	}
 
-	private void movePlayers() {
+	@Override  // implementing method from EventHandler interface 
+	public void handle(KeyEvent event) {
+		switch(event.getCode()) {
+			case DOWN :
+				this.getPacman().setDown();
+				break; 
+
+			case UP :
+				this.getPacman().setUp();
+				break;
+
+			case LEFT :
+				this.getPacman().setLeft();
+				break; 
+
+			case RIGHT :
+				this.getPacman().setRight();
+				break;
+
+			case ESCAPE:
+				ScreenSetting.getInstance().closeApplication();
+				break;
+
+			case ENTER:
+				if (this.gameOver)
+					ScreenSetting.getInstance().setMenu();
+				break;
+		}
+	}
+
+	@Override  // implementing method from Iscene interface 
+	public Scene getScene() {
+		return this.gameScene;
+	}
+
+	private void movePlayers() throws ScreenException {
 		if (this.getPacman().isAlive() && this.gameMap.hasFood()) {
 			this.getPacman().move(this.gameMap);
 
@@ -67,16 +111,15 @@ public class GameScene{
 				tmpTile.setCleared();
 				this.gameMap.eatFood();
 			}
-			
-			for (int i = 1;  i < 5; i++) {
-				if(this.getPacman().touchedGhosts((Ghost) gamePlayers[i])) {
-					this.getPacman().setDeath();
-				} else {
+
+			if(this.getPacman().touchedGhosts(gamePlayers)) 
+				this.getPacman().setDeath();
+			else {
+				for (int i = 1;  i < 5; i++) 
 					this.gamePlayers[i].move(this.gameMap);
-					
-					if(this.getPacman().touchedGhosts((Ghost) gamePlayers[i]))
-						this.getPacman().setDeath();
-				}
+				
+				if(this.getPacman().touchedGhosts(gamePlayers))
+					this.getPacman().setDeath();
 			}
 		}
 		else {
@@ -84,13 +127,8 @@ public class GameScene{
 		}
 	}
 
-	public boolean gameOver() {
-		return this.gameOver;
-	}
-
-	private void setPlayers(String mapString) {
+	private void setPlayers(String mapString) throws ScreenException {
 		int index = 1;
-
 		gamePlayers = new Player[5];
 
 		try (Scanner sc = new Scanner(new File(mapString))){
@@ -107,34 +145,29 @@ public class GameScene{
 							break;
 
 						case 'R':
-							gamePlayers[index++] = new Ghost(j, i, Preferences.RED_GHOST_SRC);
+							gamePlayers[index++] = new RedGhost(j, i);
 							break;
 
 						case 'G':
-							gamePlayers[index++] = new Ghost(j, i, Preferences.GREEN_GHOST_SRC);
+							gamePlayers[index++] = new GreenGhost(j, i);
 							break;
 
 						case 'B':
-							gamePlayers[index++] = new Ghost(j, i, Preferences.BLUE_GHOST_SRC);
+							gamePlayers[index++] = new BlueGhost(j, i);
 							break;
 
 						case 'Y':
-							gamePlayers[index++] = new Ghost(j, i, Preferences.YELLOW_GHOST_SRC);
+							gamePlayers[index++] = new YellowGhost(j, i);
 							break; 
 					}
 				}
 			}
 		} catch (Exception e) {
-		    System.out.println(e);
+		    throw new ScreenException(e.toString());
 		}
 	}
 
-	public Pacman getPacman() {
-		return (Pacman) this.gamePlayers[0];
-	}
-
-	public void setScene(Stage stage) {
-		stage.setScene(this.gameScene);
-		stage.show(); 
+	private Pacman getPacman() {
+		return (Pacman) this.gamePlayers[0]; // Pacman is always the first object of the vector (pattern)
 	}
 }
