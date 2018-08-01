5ac94267b82faa0f893233d6f05a9333d4ff3263
igornerest <igornerest@gmail.com>
2018-08-01 00:25:09 -0300

    add isAlive attribute. Changed touched Ghosts() arguments

--- engine/Pacman.java
+++ engine/Pacman.java
@@ -1,19 +1,40 @@
 package engine; 
 
+import scenerendering.ScreenException;
+
 public class Pacman extends Player {
+	private boolean isAlive;
 
-	public Pacman(int xPos, int yPos) {
-		super(Preferences.PACMAN_SRC, xPos, yPos);
+	public Pacman(int xPos, int yPos) throws ScreenException {
+		super(xPos, yPos, "./images/pacman.png");
+		this.isAlive = true;
 	}
 
-	private boolean hasSamePosition(Ghost ghost) {
-		return this.getXPos() == ghost.getXPos() && this.getYPos() == ghost.getYPos();
+	@Override
+	public void move(Map map) {
+		if (canMove(map, super.getNextMovement())) {
+			makeMovement(super.getNextMovement());
+		} else if (canMove(map, super.getMovement())) {
+			makeMovement(super.getMovement());
+		}
 	}
 
-	public boolean touchedGhosts(Ghost ... ghosts) {
-		for(Ghost ghost : ghosts)
-			if(this.hasSamePosition(ghost))
-				return true;
+	public boolean isAlive() {
+		return this.isAlive;
+	}
+
+	public void setDeath() {
+		this.isAlive = false;
+	}
+
+	public boolean touchedGhosts(Player ... comparedPlayers) {
+		for(Player comparedPlayer : comparedPlayers)
+			if(!(comparedPlayer instanceof Pacman) && this.hasSamePosition(comparedPlayer))
+					return true;
 		return false;
+	}
+
+	private boolean hasSamePosition(Player comparedPlayer) {
+		return super.getXPos() == comparedPlayer.getXPos() && super.getYPos() == comparedPlayer.getYPos();
 	}
 }
